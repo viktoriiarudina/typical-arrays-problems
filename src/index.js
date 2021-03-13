6ddace99f414fb1ae92525f487618fafe1a99034
Viktoriia <viktoria.nosik@gmail.com>
2021-03-13 12:58:58 +0100

    finish task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+    return typeof array !== 'undefined' && array.length > 0 ?  Math.min( ...array ) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    return typeof array !== 'undefined' && array.length > 0 ? Math.max( ...array ) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    return typeof array !== 'undefined' && array.length > 0 ? array.reduce((a, b) => a + b) / array.length : 0;
 }
